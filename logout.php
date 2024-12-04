6dcc96fc006da68a64cbbae59bea96501d0ea53a
Yazid Ilmi Ihsan <151598845+yazidtamvan@users.noreply.github.com>
2024-12-04 19:55:57 +0700

    Delete logout.php

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); // Menghapus semua variabel sesi
-session_destroy(); // Menghancurkan sesi
-header("Location: index.html"); // Redirect ke halaman login
-exit;
-?>
